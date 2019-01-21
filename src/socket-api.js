738a53f6953c68a31608eab5ce280ca4f3422297
Massimiliano <masdelmon@hotmail.it>
2019-01-21 01:35:00 +0100

    Update socket-api.js

--- src/socket-api.js
+++ src/socket-api.js
@@ -1,6 +1,6 @@
 import clientSocket from 'socket.io-client';
 
-export const API_URL = "http://whatever:5555";
+export const API_URL = "https://route5eqvtwr2-masdelmon2-che.4e1e.starter-us-east-1b.openshiftapps.com:3000";
 const socket = clientSocket(`${API_URL}/calc`);
 export const subscribe = (newCallback) => {
   socket.on("calc", (result) => {
